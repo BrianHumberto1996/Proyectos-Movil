d7c3d32b58da91741c4a9a26b6a0358713369dfc
Unknown <s14120038@alumnos.itsur.edu.mx>
2018-02-28 23:18:39 -0600

    Uso de volley para consumir servicios

---
+++ Volley/app/src/main/java/com/example/humberto/volley/actividadimagen.java
@@ -0,0 +1,12 @@
+package com.example.humberto.volley;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actividadimagen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_actividadimagen);
+    }
+}
